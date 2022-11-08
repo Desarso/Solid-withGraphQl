9815ea59ea38c4c16987c28e7b432d9fd531116b
Desarso <malek.gabriel33@gmail.com>
2022-11-08 12:34:19 -0800

    all

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,32 @@
-import { Component, createSignal, For } from "solid-js";
-import { createClient, subscriptionExchange, defaultExchanges } from "@urql/core";
-import { createResource } from "solid-js";
-import { SubscriptionClient } from "subscriptions-transport-ws";
-import { pipe, subscribe } from "wonka";
+import { For } from "solid-js/web";
+import { Component, createResource, createSignal} from "solid-js";
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
+
 
 // const subscriptionClient = new SubscriptionClient("ws://localhost:4000/graphql", {
 //   reconnect: true,
 // });
 
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  // exchanges: [
-  //   ...defaultExchanges,
-  //   subscriptionExchange({
-  //     forwardSubscription: (operation) => 
-  //     subscriptionClient.request(operation),
-  //   }),
-  // ]
-
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
 });
+
+const Get_Todos = gql`
+  query getTodos {
+    todos {
+      id
+      text
+      done 
+    }
+  }`;
 
 
 const [todos, { refetch }] = createResource(() =>
   client
-    .query(
-      `
-      query{
+    .query({
+     query: gql`
+      query name{
         getTodos{
           id 
           done
@@ -33,87 +34,80 @@ const [todos, { refetch }] = createResource(() =>
         }
       }
       `,
-      { id: "test" }
-    )
-    .toPromise()
-    .then((req) => {
-      // console.log(req.operation.context.meta?.cacheOutcome);
-      // console.log(req);
-      if (req.operation.context.meta?.cacheOutcome === "hit") {
-        return [];
-      }
-      return req.data.getTodos;
+    })
+    .then((result) => {
+      console.log(result);
+      return result.data.getTodos;
     })
 );
 
 const App: Component = () => {
   const [text, setText] = createSignal("");
 
-  const toggle = async (id: string) => {
+  const toggle = async (id: any, done: boolean) => {
     await client
-      .mutation(
-        `
-      mutation($id: ID!, $done: Boolean!){
+      .mutate({
+         mutation: gql`
+      mutation ($id: ID!, $done: Boolean!){
         setDone(id: $id, done: $done){
           id
         }
       }
       `,
-        {
-          id,
-          done: !todos().find((todo: any) => todo.id === id).done,
-        }
-      )
-      .toPromise();
+          variables: {
+            id: id,
+            done: !done,
+      }
+      })
     refetch();
   };
 
-  const onDelete = async (id: string) => {
-    await client
-      .mutation(
-        `
-      mutation($id: ID!){
-        deleteTodo(id: $id){
-          id
-        }
-      }
-      `,
-        {
-          id,
-        }
-      )
-      .toPromise();
-    await refetch();
-  };
+  // const onDelete = async (id: string) => {
+  //   await client
+  //     .mutation(
+  //       `
+  //     mutation($id: ID!){
+  //       deleteTodo(id: $id){
+  //         id
+  //       }
+  //     }
+  //     `,
+  //       {
+  //         id,
+  //       }
+  //     )
+  //     .toPromise();
+  //   await refetch();
+  // };
 
-  const onAdd = async () => {
-    await client
-      .mutation(
-        `
-      mutation($text: String!){
-        addTodo(text: $text){
-          text
-          id
-        }
-      }
-      `,
-        {
-          text: text(),
-        }
-      )
-      .toPromise();
-    refetch();
-    setText("");
-  };
+  // const onAdd = async () => {
+  //   await client
+  //     .mutation(
+  //       `
+  //     mutation($text: String!){
+  //       addTodo(text: $text){
+  //         text
+  //         id
+  //       }
+  //     }
+  //     `,
+  //       {
+  //         text: text(),
+  //       }
+  //     )
+  //     .toPromise();
+  //   refetch();
+  //   setText("");
+  // };
 
   return (
     <div>
       <For each={todos()}>
-        {({ id, done, text }: { id: string; done: boolean; text: string }) => (
+        {({ id, done, text }: { id: any; done: boolean; text: string }) => (
           <div>
-            <input type="checkbox" checked={done} onClick={() => toggle(id)} />
+            <input type="checkbox" checked={done} onClick={() => toggle(id, done)} />
             <span>{text}</span>
-            <button onclick={() => onDelete(id)}>Delete Me</button>
+            <button onclick={() => console.log("implement deletion")}>Delete Me</button>
           </div>
         )}
       </For>
@@ -122,7 +116,7 @@ const App: Component = () => {
         value={text()}
         oninput={(evt) => setText(evt.currentTarget.value)}
       />
-      <button onclick={onAdd}>Add</button>
+      <button onclick={() => console.log("implement mutations")}>Add</button>
     </div>
   );
 };
